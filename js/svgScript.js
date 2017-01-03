6a10319c9f6f67f7750bb7ab2a6d482eb85480f1
Matthew Kaufer <mjkaufer@gmail.com>
2017-01-03 01:57:29 -0500

    Minor improvements to bear svg, added bear svg to html, added colors to pages, fixed some resulting CSS bugs

--- js/svgScript.js
+++ js/svgScript.js
@@ -3,9 +3,9 @@ var applyPage = Snap("#applyPage");
 
 
 // define the url to load the svg from
-var url = "assets/hackgteeny-fixed.svg";
+var splashUrl = "assets/hackgteeny-fixed.svg";
 
-Snap.load(url, function(fragment) {
+Snap.load(splashUrl, function(fragment) {
 	var svgElement = fragment.select("svg");
 	console.log(svgElement)
 	svgElement.attr({
@@ -115,3 +115,31 @@ function prepareApplyPage(){
 	}
 	window.requestAnimationFrame(update);
 }
+
+
+var bear = Snap("#bear");
+
+
+// define the url to load the svg from
+var bearUrl = "assets/SleepingBear.svg";
+
+Snap.load(bearUrl, function(fragment) {
+	var svgElement = fragment.select("svg");
+	console.log(svgElement)
+	svgElement.attr({
+		id: "bear_svg"
+	});
+
+	window.onresize = function() {
+		//this way when we change the screen size the SVG changes size too
+		svgElement.attr({
+			//height: window.innerHeight,
+		});
+	}
+
+	bear.append(svgElement);
+	// prepareApplyPage()
+})
+
+
+
